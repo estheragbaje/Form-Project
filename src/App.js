4fd8b3af172bbbf680b35ca313a8ab08a52d7716
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-05-23 21:10:10 +0100

    chore: set up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Form from './components/Form';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Form} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
     </div>
   );
 }
