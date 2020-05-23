25febbd5df033a8f30d581e1dcf024bf1f5872e2
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-05-23 20:37:31 +0100

    chore: created initial values and form validations using yup

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import { Box, Button } from '@chakra-ui/core';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import InputForm from './InputForm';
+
+const initialValues = {
+  name: '',
+  email: '',
+  phone: '',
+  password: '',
+  confirmPassword: '',
+  pin: '',
+};
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required().min(2, 'Name is too short'),
+  email: yup.string().email().required(),
+  phone: yup.string().required(),
+  password: yup
+    .string()
+    .required('Please enter your password')
+    .matches(
+      /^.*(?=.{6,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      'Password must contain at least 6 characters, one uppercase, one number and one special case character'
+    ),
+  confirmpassword: yup
+    .string()
+    .required('Please confirm your password')
+    .when('password', {
+      is: (val) => (val && val.length > 0 ? true : false),
+      then: yup
+        .string()
+        .oneOf([yup.ref('password')], 'Needs to be the same as password value'),
+    }),
+  pin: yup
+    .string()
+    .required()
+    .matches(/^[0-9]+$/, 'Must be only digits')
+    .min(4, 'Must be exactly 4 digits')
+    .max(4, 'Must be exactly 4 digits'),
+});
 
 function Form() {
   return (
