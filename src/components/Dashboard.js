cbc935eed5bfafb03e9ccd1e14e55b8e277cfb47
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-05-23 21:07:22 +0100

    chore: created Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/core';
+
+function Dashboard() {
+  return (
+    <Box>
+      <Heading>Welcome to Softcom!</Heading>
+    </Box>
+  );
+}
+
+export default Dashboard;
